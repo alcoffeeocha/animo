6f3265553d20f63850730c875c98789d3454e25f
alcoffeeocha <alkahfikhuzaimy@gmail.com>
2021-06-18 08:12:21 +0700

    Add Resep Home

--- dist/resep.ae5dbd42.js
+++
@@ -1,2 +0,0 @@
-document.querySelector("[data-link^=resep]").classList.add("sidebar__link--active"),document.getElementById("header-title").textContent="Resep";
-//# sourceMappingURL=resep.ae5dbd42.js.map
